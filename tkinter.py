a539cca048459871f67722ac8f67ff104e1f7e9e
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-06-12 15:14:13 -0400

    Page 13

--- tkinter.py
+++ tkinter.py
@@ -2,8 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 
 win = tk.Tk()
+
 win.title("Python GUI")
 
-ttk.Label(win,text = 'A label').grid(column=0,row=0)
+win.resizable(True, True)
 
 win.mainloop()
