985dac77357d81df9594eb16599760f7b6087dd9
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-06-13 15:10:01 -0400

    Page 64: Top-level frame and label

--- GUI_tabbed.py
+++ GUI_tabbed.py
@@ -5,11 +5,18 @@ def main():
   window = tk.Tk()
   window.title("Python GUI")
 
+
   # Tab controls, p. 63
   tabControl = ttk.Notebook(window)
   
   tab1 = ttk.Frame(tabControl)
   tabControl.add(tab1, text='Tab 1')
+
+  mighty = ttk.LabelFrame(tab1, text=" Mighty Python") # p. 64
+  mighty.grid(column = 0, row = 0, padx=8, pady=4)
+
+  a_label = ttk.Label(mighty, text="Enter a name:")
+  a_label.grid(column = 0, row = 0, sticky = 'W')
   
   tab2 = ttk.Frame(tabControl)
   tabControl.add(tab2, text='Tab 2')
