5edf2a23001d6e4d029405d89c0db468767a50ff
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-06-13 14:10:40 -0400

    Page 48

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def main():
 
   button = ttk.Button(win, text='Click me!', command=click_me)
   button.grid(column=2, row=1)
+  button.focus() # Bring focus to textbox as soon as program runs (p. 22)
 
   # Dropdown menu (p. 24)
   ttk.Label(win, text="Choose a number:").grid(column=1,row=0)
@@ -39,9 +40,9 @@ def main():
   chVarDis = tk.IntVar()
   check1 = tk.Checkbutton(win, text="Disabled", variable=chVarDis, state='disabled')
   check1.select()
-  check1.grid(column=0, row=4,)
+  # check1.grid(column=0, row=4,)
   # The `sticky=tk.W` appears to change the margin between the element and the window border
-  # check1.grid(column=0, row=4, sticky=tk.W)
+  check1.grid(column=0, row=4, sticky=tk.W)
 
   chVarUn = tk.IntVar()
   check2 = tk.Checkbutton(win, text="Unchecked", variable=chVarUn)
@@ -76,34 +77,33 @@ def main():
   rad2 = tk.Radiobutton(win, text=Blue, variable=radVar, value=2, command=radCall)
   rad3 = tk.Radiobutton(win, text=Gold, variable=radVar, value=3, command=radCall)
   
-  rad1.grid(column=0, row=5)
-  rad2.grid(column=1, row=5)
-  rad3.grid(column=2, row=5)
+  # rad1.grid(column=0, row=5)
+  # rad2.grid(column=1, row=5)
+  # rad3.grid(column=2, row=5)
 
-  # rad1.grid(column=0, row=5, sticky=tk.W, columnspan=3)
-  # rad2.grid(column=1, row=5, sticky=tk.W, columnspan=3)
-  # rad3.grid(column=2, row=5, sticky=tk.W, columnspan=3)
+  rad1.grid(column=0, row=5, sticky=tk.W, columnspan=3)
+  rad2.grid(column=1, row=5, sticky=tk.W, columnspan=3)
+  rad3.grid(column=2, row=5, sticky=tk.W, columnspan=3)
 
   # Textbox with scrollbar, p. 32
   textbox = scrolledtext.ScrolledText(win, width=30, height=5, wrap = tk.WORD)
-  textbox.grid(column=0, columnspan=3)
+  textbox.grid(column=0) #, columnspan=3)
 
   # Labels, p. 38
-  buttons_frame = ttk.LabelFrame(win, text='')
-  buttons_frame.grid(column=0, row=7, padx=20, pady=40)
-  ttk.Label(buttons_frame, text='Label1 - Once upon a midnight dreary, while I pondered weak and weary').grid(column=0, row=0, sticky=tk.W)
-  ttk.Label(buttons_frame, text='Label2').grid(column=0, row=1, sticky=tk.W)
-  ttk.Label(buttons_frame, text='Label3').grid(column=0, row=2, sticky=tk.W)
+  buttons_frame = ttk.LabelFrame(win, text=' Labels in a frame   ')
+  buttons_frame.grid(column=0, row=7, padx=10, pady=10) #, columnspan=3)
+  ttk.Label(buttons_frame, text='Label1').grid(column=0, row=0, sticky=tk.W)
+  ttk.Label(buttons_frame, text='Label2').grid(column=1, row=0, sticky=tk.W)
+  ttk.Label(buttons_frame, text='Label3').grid(column=2, row=0, sticky=tk.W)
 
   # - `winfo_children()` method returns a list of all children, (p. 43)
   # - `grid_configure()` method allows modification of UI elements before 
   #    the main loop displays them
-  for i in buttons_frame.winfo_children():
-    i.grid_configure(padx=8, pady=4)
+  # for i in buttons_frame.winfo_children():
+  #   i.grid_configure(padx=8, pady=4)
 
-  # Bring focus to textbox as soon as program runs (p. 22)
-  button.focus()
-  win.resizable(False, False)
+
+  win.resizable(True, True)
   win.mainloop()
 
 if __name__ == '__main__':
