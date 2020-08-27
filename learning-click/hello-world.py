45be3d9880819b207607434719714346e1105cad
jasper-zanjani <jasper.zanjani@gmail.com>
2020-08-27 10:44:58 -0400

    :snake::raising_hand: Hello World in click

---
+++ learning-click/hello-world.py
@@ -0,0 +1,9 @@
+import click
+
+@click.command()
+@click.option('--arg',default='world', help = 'Specify who to say hello to!', prompt="What is your name? ")
+def hello(arg):
+    print(f'Hello {arg}!')
+
+if __name__ == '__main__':
+    hello()
