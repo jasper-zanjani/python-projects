4dc23be0870b89baa3adffa3c74d8e3c2e89a806
jasper-zanjani <jasper.zanjani@gmail.com>
2020-08-29 18:28:50 -0400

    :hammer_and_wrench: refactor csv2json

--- csv2json/csv2json.py
+++ csv2json/csv2json.py
@@ -2,18 +2,24 @@ import json
 import csv
 import click
 
-data = []
+def read(filename):
+    output = list()
+    with open(filename,'r') as f:
+        cr = csv.reader(f)
+        headers = next(cr)
+        output.append(dict(zip(headers,next(cr))))
+    return output
+
+def write(data,filename):
+    with open(filename,'w') as f:
+        json.dump(data,f)
 
 @click.command()
 @click.argument('filename')
 def main(filename):
-    with open(filename,'r') as f:
-        cr = csv.reader(f)
-        headers = next(cr)
-        data.append(dict(zip(headers,next(cr))))
-    output = filename.replace('csv','json')
-    with open(output,'w') as f:
-        json.dump(data,f)
+    data = read(filename)
+    output_filename = filename.replace('csv','json')
+    write(data,output_filename)
 
 if __name__ == '__main__':
     main()
