159a73e68e7576681a69f6f725f08a45a09bc7e4
jasper-zanjani <jasper.zanjani@gmail.com>
2020-08-29 18:56:56 -0400

    :hammer_and_wrench: globbing

--- csv2json/csv2json.py
+++ csv2json/csv2json.py
@@ -1,6 +1,8 @@
 import json
 import csv
 import click
+import glob
+import os.path
 
 def read(filename):
     output = list()
@@ -8,18 +10,29 @@ def read(filename):
         cr = csv.reader(f)
         headers = next(cr)
         output.append(dict(zip(headers,next(cr))))
+        print(f'{filename} read')
     return output
 
 def write(data,filename):
     with open(filename,'w') as f:
         json.dump(data,f)
+        print(f'{filename} written')
 
 @click.command()
 @click.argument('filename')
 def main(filename):
-    data = read(filename)
-    output_filename = filename.replace('csv','json')
-    write(data,output_filename)
+    print(f'Received argument {filename}')
+    files = list()
+    if filename.find( '*') > 0:
+        print('Initiating globbing')
+        files = [os.path.abspath(f) for f in glob.glob(os.path.expanduser(filename))]
+    else:
+        files = [filename]
+    print(f'Converting {len(files)} files ')
+    for f in files:
+        data = read(f)
+        output_filename = f.replace('csv','json')
+        write(data,output_filename)
 
 if __name__ == '__main__':
     main()
