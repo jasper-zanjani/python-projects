7a4621306351f5330f95faa00fff73aa861c3c22
jasper-zanjani <jasper.zanjani@gmail.com>
2020-09-01 15:30:01 -0400

    :snake::robot: Basic bot

---
+++ Discord/Bot.py
@@ -0,0 +1,27 @@
+import discord
+import os
+import dotenv
+
+dotenv.load_dotenv()
+token = os.getenv('TOKEN')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.channel.send(f'Hello {message.author.mention}')
+        elif message.content.startswith('!bot'):
+            await message.channel.send(f':wave:')
+
+client = MyClient()
+client.run(token)
+
