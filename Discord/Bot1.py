789ef25f3365e12a97383c7570a79698f8f493b7
jasper-zanjani <jasper.zanjani@gmail.com>
2020-09-01 15:31:54 -0400

    :snake::robot::video_game:

--- Discord/Bot1.py
+++ Discord/Bot1.py
@@ -15,4 +15,8 @@ async def eight_ball(ctx):
     possible_responses = ':smiley: Once upon a midnight dreary, :grin: While I pondered weak and weary, :laughing: Over many a quaint and curious volume of forgotten lore, :joy: While I nodded nearly napping, :rofl: Suddenly there came a rapping, :heart_eyes: As of someone gently tapping, :relieved: Tapping at my chamber door.'.split(", ")
     await ctx.send(f'{random.choice(possible_responses)}, {ctx.message.author.mention}')
 
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(name="Fall Guys"))
+
 client.run(token)
