d92d9553a34dc96e4e6e64a86898f92e039d2b44
jasper-zanjani <jasper.zanjani@gmail.com>
2020-09-02 16:46:14 -0400

    :snake::robot::file_folder: cogs

--- Discord/cogs.py
+++ Discord/cogs.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import dotenv
 import os
 import glob
+import os.path
 
 dotenv.load_dotenv()
 token = os.getenv('TOKEN')
@@ -13,17 +14,19 @@ client = commands.Bot(command_prefix='.')
 @client.command()
 async def load(ctx, extension):
   client.load_extension(f'cogs.{extension}')
+  print('Bot loaded')
 
 @client.command()
 async def unload(ctx, extension):
   client.unload_extension(f'cogs.{extension}')
 
-# for f in os.listdir(''):
+# for f in os.listdir('./cogs'):
 #   if f.endswith('.py'):
 #     client.load_extension(f'cogs.{f[:-3]}')
 
-cogs = [f[:-3] for f in glob.glob('./cogs/*.py')]
-for c in cogs:
-  client.load_extension(f'cogs.{c}')
+# cogs = [os.path.basename(f)[:-3] for f in glob.glob('./cogs/*.py')]
+# for c in cogs:
+#   client.load_extension(f'cogs.{c}')
 
+client.load_extension('cogs.Admin')
 client.run(token)
