8e90a1ab95c69c5ff661be94dc0af341cafbb739
jasper-zanjani <jasper.zanjani@gmail.com>
2020-09-05 12:55:12 -0400

    :heavy_plus_sign:

--- Discord/cogs/Example.py
+++ Discord/cogs/Example.py
@@ -1,4 +1,5 @@
 import discord
+import pickle
 from discord.ext import commands
 
 class Example(commands.Cog):
@@ -8,10 +9,19 @@ class Example(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print('Bot is online')
+        guilds = self.client.guilds
+        print([g.name for g in guilds])
+
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send('Pong!')
+        await ctx.send(f'https://www.google.com')
 
-def setup(client):
-    client.add_cog(Example(client)) 
+    @commands.command()
+    async def pickleme(self, ctx):
+        with open('pickle','wb') as f:
+            pickle.dump(ctx, f)
+        return 0
+
+def setup(c):
+    c.add_cog(Example(c)) 
--- Discord/cogs/Guild.py
+++ Discord/cogs/Guild.py
@@ -6,8 +6,8 @@ class Guild(commands.Cog):
     self.client = client
 
   @commands.command()
-  async def guildinfo(self, ctx, guild: discord.Guild = None ):
-    guild = ctx.guild if not guild else guild
+  async def guildinfo(self, ctx):
+    guild = ctx.guild
     embed = discord.Embed(title=f'Information about guild {guild}',
                           description="Description here",
                           timestamp= ctx.message.created_at,
@@ -15,6 +15,13 @@ class Guild(commands.Cog):
                           # thumbnail=guild.icon_url)
                           )
     embed.set_thumbnail(url=guild.icon_url)
+    await ctx.send(embed=embed)
+
+  @commands.command()
+  async def channels(self, ctx):
+    channels = [c.name for c in ctx.guild.channels if type(c) is discord.TextChannel]
+    # print(channels)
+    embed = discord.Embed(title = f'{ctx.guild.name} channels', description = '**Channels:**\n- ' + "\n- ".join(channels))
     await ctx.send(embed=embed)
 
 def setup(client):
