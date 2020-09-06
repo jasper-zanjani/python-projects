5a16cd6b03143db05b78dbe7a224f4547b7238eb
jasper-zanjani <jasper.zanjani@gmail.com>
2020-09-06 01:47:57 -0400

    :snake::robot: Reaction roles

--- Discord/reaction-roles.py
+++ Discord/reaction-roles.py
@@ -1,5 +1,6 @@
 import os, dotenv, discord
 from discord.ext import commands
+from discord.utils import find, get
 
 dotenv.load_dotenv()
 token=os.getenv('TOKEN')
@@ -9,9 +10,21 @@ client = commands.Bot(command_prefix='.')
 @client.event
 async def on_raw_reaction_add(payload):
   if payload.message_id == 751854496748929065 and payload.emoji.name == u"\U0001F44D":
-    guild = discord.utils.find(lambda g : g.id == payload.guild_id, client.guilds)
-    role = discord.utils.get(guild.roles, name='Goombalee')
-    member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+    # Original
+    # guild = discord.utils.find(lambda g : g.id == payload.guild_id, client.guilds)
+    # Simpler
+    # guild = client.get_guild(payload.guild_id)
+
+    # Alternative
+    guild = get(client.guilds, id=payload.guild_id)
+
+    
+    role = get(guild.roles, name='Goombalee')
+
+    # Original
+    # member = find(lambda m : m.id == payload.user_id, guild.members)
+    # Simpler
+    member = get(guild.members, id=payload.user_id)
     await member.add_roles(role)
 
 
