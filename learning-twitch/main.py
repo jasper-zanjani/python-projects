826c94690debe4d8b8980d0fafa68dc3a06be12a
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-15 16:35:40 -0400

    Twitch

--- learning-twitch/main.py
+++ learning-twitch/main.py
@@ -9,12 +9,51 @@ client_id = os.getenv('CLIENT_ID')
 bot = commands.Bot(
   irc_token = token,
   client_id=client_id,
-  prefix=',',
-  nick=''
+  prefix='.',
+  nick='realbeardbae',
+  initial_channels=['realbeardbae']
+
 )
 
-@bot.command()
-async def hello(self, ctx):
-  await ctx.send(f'Hello {ctx.author.name}!')
+# @bot.command()
+# async def hello(self, ctx):
+#   await ctx.send(f'Hello {ctx.author.name}!')
+
+# bot.run()
+
+# Example from docs:
+# class Bot(commands.Bot):
+#  def __init__(self):
+#    super().__init__(irc_token=token, client_id=client_id, nick='realbeardbae', prefix='!', initial_channels=['gmhikaru'])
+#  async def event_ready(self):
+#      print(f'Ready | {self.nick}')
+#  async def event_message(self, message):
+#      print(message.content)
+#      await self.handle_commands(message)
+#  @commands.command(name='test')
+#  async def my_command(self, ctx):
+#      await ctx.send(f'Hello {ctx.author.name}!')
+
+# bot = commands.Bot(
+#   irc_token='...',
+#   api_token='test',
+#   nick='mysterialpy',
+#   prefix='!',
+#   initial_channels=['mysterialpy']
+# )
+
+@bot.event
+async def event_ready():
+  print(f'Ready | {bot.nick}')
+
+@bot.event
+async def event_message(message):
+  print(message.content)
+  await bot.handle_commands(message)
+
+
+@bot.command(name='test', aliases=['t'])
+async def test_command(ctx):
+    await ctx.send(f'Hello {ctx.author.name}')
 
 bot.run()
