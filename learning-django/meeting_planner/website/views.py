496ee53733f7b2a0f0f8f90d76f588705fbcca75
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-16 19:12:13 -0400

    :snake::calendar: welcome and date defs

--- learning-django/meeting_planner/website/views.py
+++ learning-django/meeting_planner/website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+def welcome(request):
+  return HttpResponse("Welcome to the Meeting Planner!")
+
+def date(req):
+  return HttpResponse(f'This page was served at {datetime.now()}')
