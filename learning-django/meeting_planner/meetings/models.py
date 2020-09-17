777d7ce95c2c09811d4674c40b609e5b6c753d74
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-17 10:00:51 -0400

    :snake::calendar: Room model

--- learning-django/meeting_planner/meetings/models.py
+++ learning-django/meeting_planner/meetings/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from datetime import time
 
 # Create your models here.
 class Meeting(models.Model):
   title = models.CharField(max_length=200)
   date = models.DateField()
+  start_time = models.TimeField(default=time(9))
+  duration = models.IntegerField(default=1)
+
+  def __str__(self):
+    return f'{self.title} at {self.start_time} on {self.date}'
+
+class Room(models.Model):
+  name = models.CharField(max_length=128)
+  floor = models.IntegerField()
+  room = models.IntegerField()
+
+  def __str__(self):
+    return f'{self.name}: {self.floor}.{self.room}'
