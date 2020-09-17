f2e79097555dd4de46fb6d963a6c5489de4634c9
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-17 15:26:38 -0400

    :snake::calendar: Submit button

--- learning-django/meeting_planner/meetings/views.py
+++ learning-django/meeting_planner/meetings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.forms import modelform_factory
 from .models import Meeting, Room
 
@@ -13,5 +13,12 @@ def rooms(req):
 MeetingForm = modelform_factory(Meeting, exclude=[])
 
 def new(request):
-  form = MeetingForm()
+  if request.method == 'POST':
+    form = MeetingForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect("home")
+  else:
+    form = MeetingForm()
+  
   return render(request, "meetings/new.html", {"form": form})
