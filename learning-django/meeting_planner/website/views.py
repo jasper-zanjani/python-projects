9665800d9b850edc9394d99eddf1b18d4f3496f9
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-17 11:09:39 -0400

    :snake::calendar: Template context

--- learning-django/meeting_planner/website/views.py
+++ learning-django/meeting_planner/website/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from meetings.models import Meeting
 
-# Create your views here.
 def welcome(request):
-  return render(request,"website/welcome.html")
+  dict_ = {"num_meetings": Meeting.objects.count()}
+  return render(request,"website/welcome.html", dict_)
 
 def date(req):
   return HttpResponse(f'This page was served at {datetime.now()}')
