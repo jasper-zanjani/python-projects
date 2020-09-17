981fe6b3172243d5f616162455929965246ea03c
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-17 10:27:29 -0400

    :snake::calendar: Room model

--- learning-django/meeting_planner/meetings/admin.py
+++ learning-django/meeting_planner/meetings/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Meeting
+from .models import Meeting, Room
 admin.site.register(Meeting)
+admin.site.register(Room)
--- learning-django/meeting_planner/meetings/models.py
+++ learning-django/meeting_planner/meetings/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from datetime import time
 
 # Create your models here.
-class Meeting(models.Model):
-  title = models.CharField(max_length=200)
-  date = models.DateField()
-  start_time = models.TimeField(default=time(9))
-  duration = models.IntegerField(default=1)
-
-  def __str__(self):
-    return f'{self.title} at {self.start_time} on {self.date}'
 
 class Room(models.Model):
   name = models.CharField(max_length=128)
@@ -18,3 +10,13 @@ class Room(models.Model):
 
   def __str__(self):
     return f'{self.name}: {self.floor}.{self.room}'
+
+class Meeting(models.Model):
+  title = models.CharField(max_length=200)
+  date = models.DateField()
+  start_time = models.TimeField(default=time(9))
+  duration = models.IntegerField(default=1)
+  room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.title} at {self.start_time} on {self.date}'
