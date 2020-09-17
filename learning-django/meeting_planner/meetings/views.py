d2f384e54e77c663bd998b9917646fdd00911905
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-17 15:08:56 -0400

    :snake::calendar: ModelForm

--- learning-django/meeting_planner/meetings/views.py
+++ learning-django/meeting_planner/meetings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.forms import modelform_factory
 from .models import Meeting, Room
 
 def detail(request, id):
@@ -8,3 +9,9 @@ def detail(request, id):
 
 def rooms(req):
   return render(req, 'meetings/rooms.html', {'rooms': Room.objects.all()})
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+  form = MeetingForm()
+  return render(request, "meetings/new.html", {"form": form})
