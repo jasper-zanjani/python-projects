b5f5548f597639a31743a749dfa8a4abf937973a
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-17 11:42:52 -0400

    :snake::calendar: 404 error

--- learning-django/meeting_planner/meetings/views.py
+++ learning-django/meeting_planner/meetings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting
 
 def detail(request, id):
-  meeting = Meeting.objects.get(pk=id)
+  # meeting = Meeting.objects.get(pk=id)
+  meeting = get_object_or_404(Meeting, pk=id)
   return render(request, "meetings/detail.html", {"meeting": meeting})
