adbd37fc4e47cdd6e51563c94d57f062ab8b8f27
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-17 12:48:24 -0400

    :snake::calendar: meetings urls file

--- learning-django/meeting_planner/meeting_planner/urls.py
+++ learning-django/meeting_planner/meeting_planner/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from website.views import welcome, date, about
-from meetings.views import detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome, name='home'),
-    path('mtg/<int:id>', detail, name='detail'),
+    path('mtg/', include('meetings.urls')),
 ]
--- learning-django/meeting_planner/website/views.py
+++ learning-django/meeting_planner/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from meetings.models import Meeting
+from meetings.models import Meeting, Room
 
 def welcome(request):
   return render(request,"website/welcome.html", {"meetings": Meeting.objects.all()})
