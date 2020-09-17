e9f7a01e5993649752aa23af4698f17b854d5a30
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-17 11:40:15 -0400

    :snake::calendar: Detail page

--- learning-django/meeting_planner/meeting_planner/urls.py
+++ learning-django/meeting_planner/meeting_planner/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from website.views import welcome, date, about
+from meetings.views import detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome)
+    path('', welcome),
+    path('meetings/<int:id>', detail),
 ]
--- learning-django/meeting_planner/meetings/views.py
+++ learning-django/meeting_planner/meetings/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Meeting
 
-# Create your views here.
+def detail(request, id):
+  meeting = Meeting.objects.get(pk=id)
+  return render(request, "meetings/detail.html", {"meeting": meeting})
