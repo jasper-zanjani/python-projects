9bd19e88dcdd480551532315b7b68a6b233b23af
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-18 15:00:30 -0400

    :snake::shell: PowerShell app

---
+++ learning-django/pwsh_lookup/pwsh_lookup/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pwsh_lookup project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pwsh_lookup.settings')
+
+application = get_wsgi_application()
