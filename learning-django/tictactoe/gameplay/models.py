d664b699e17ec72e73161b7614c0547ad05c9918
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-18 15:01:02 -0400

    :snake::game_die: Model fix

--- learning-django/tictactoe/gameplay/models.py
+++ learning-django/tictactoe/gameplay/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Game(models.Model):
+  first_player = models.ForeignKey(User, related_name="games_first_player", on_delete=models.CASCADE)
+  second_player = models.ForeignKey(User, related_name="games_second_player", on_delete=models.CASCADE)
+
+  start_time = models.DateTimeField(auto_now_add=True)
+  last_active = models.DateTimeField(auto_now=True)
+
 class Move(models.Model):
   x = models.IntegerField()
   y = models.IntegerField()
   comment = models.CharField(max_length=300, blank=True)
   by_first_player = models.BooleanField()
   game = models.ForeignKey(Game, on_delete=models.CASCADE)
-
-class Game(models.Model):
-  first_player = models.ForeignKey(User, related_name="games_first_player")
-  second_player = models.ForeignKey(User, related_name="games_second_player")
-
-  start_time = models.DateTimeField(auto_now_add=True)
-  last_active = models.DateTimeField(auto_now=True)
