2089b36bcc007c45380bab3d59322c5368b6d0c0
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-20 16:20:58 -0400

    :snake::heavy_check_mark: TaskForm

--- learning-django/todo/tasks/views.py
+++ learning-django/todo/tasks/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
   tasks = Task.objects.all()
-  context = {'tasks': tasks}
+
+  form = TaskForm()
+
+  context = {'tasks': tasks, 'form': form}
   return render(request, 'tasks/list.html', context)
