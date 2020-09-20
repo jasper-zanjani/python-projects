3ea009adc43fd8f8dea9b0bc93a0c0cd09ed7f51
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-09-20 15:39:34 -0400

    :snake::heavy_check_mark: Add Task model to view

--- learning-django/todo/tasks/views.py
+++ learning-django/todo/tasks/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
 def index(request):
-  return render(request, 'tasks/list.html')
+  tasks = Task.objects.all()
+  context = {'tasks': tasks}
+  return render(request, 'tasks/list.html', context)
